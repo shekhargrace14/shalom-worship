086d1582dc75b92e58260a5685d74db802bffd52
shekhargrace14 <shekhargrace14@gmail.com>
2022-10-07 23:43:57 +0530

    OK

--- pages/SongPage.js
+++ pages/SongPage.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import SongMain from '../components/SongMain'
+import Card1x3 from '../components/Card1x3'
 
 const SongPage = () => {
   return (
     <>
         <Navbar/>
         <SongMain/>
+        <Card1x3/>
         <Footer/>
     </>
   )
